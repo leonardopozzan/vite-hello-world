e97c67871bf6da6774e325294ccec6a42cb53175
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-11-09 15:47:31 +0100

    esercizio finito

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HeaderComponentVue />
+</template>
+
+<script>
+import HeaderComponentVue from './components/HeaderComponentVue.vue';
+  export default {
+    data(){
+      return{
+      paragrafo : 'lorem'
+      }
+    },
+    components:{HeaderComponentVue}
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/components/HeaderComponentVue.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1 class="title">{{titolo}}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                titolo: 'La mia prima App con Vite'
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    header{
+        height: 60vh;
+        background-color: aqua;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .title{
+        color: white;
+        padding: 10px;
+        background-color: red;
+    }
+</style>
